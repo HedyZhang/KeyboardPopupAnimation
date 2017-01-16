fbccfde06883fd3b8adc35872c8b2661ac3b651f
zhanghaidi <zhanghaidi@guangquaner.com>
2017-01-16 13:00:12 +0800

    annotation

--- Keyboard/ViewController.m
+++ Keyboard/ViewController.m
@@ -20,59 +20,50 @@
     _textField.borderStyle = UITextBorderStyleRoundedRect;
     _textField.backgroundColor = [UIColor orangeColor];
     [self.view addSubview:_textField];
-    
+    //监听键盘
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHidden:) name:UIKeyboardWillHideNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHidden:) name:UIKeyboardWillHideNotification object:nil];
     
 }
-- (void)keyboardWillShow:(NSNotification *)notification
-{
+//键盘将要显示回调
+- (void)keyboardWillShow:(NSNotification *)notification {
     NSDictionary *userInfo = notification.userInfo;
     CGRect keyboardBounds = [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
-    
-    
+    //获取键盘动画的duration
     CGFloat duration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-    
+    //获取键盘的高度，计算textField需要平移的高度
     CGFloat deltaY = keyboardBounds.size.height;
-    
-    void (^animations)() = ^{ self.textField.transform = CGAffineTransformMakeTranslation(0, -deltaY);};
+    //设置要执行动画的block
+    void (^animations)() = ^{
+        //平移TextField
+        self.textField.transform = CGAffineTransformMakeTranslation(0, -deltaY);
+    };
     
     if (duration > 0) {
         NSUInteger options = [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] unsignedIntegerValue];
         [UIView animateWithDuration:duration delay:0 options:options animations:animations completion:nil];
-    }
-    else
-    {
+    } else {
         animations();
     }
     
 }
 
-- (void)keyboardWillHidden:(NSNotification *)notification
-{
+- (void)keyboardWillHidden:(NSNotification *)notification {
     NSDictionary *userInfo = notification.userInfo;
-    
+    //获取键盘动画的duration
     CGFloat duration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-    
-    void (^animations)() = ^{ self.textField.transform = CGAffineTransformIdentity;};
+    //设置要执行动画的block
+    void (^animations)() = ^{
+        self.textField.transform = CGAffineTransformIdentity;
+    };
     
     if (duration > 0) {
         NSUInteger options = [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] unsignedIntegerValue];
         [UIView animateWithDuration:duration delay:0 options:options animations:animations completion:nil];
-    }
-    else
-    {
+    } else {
         animations();
     }
-
-    
-    
 }
-
-
-
-
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
